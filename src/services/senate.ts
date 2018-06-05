9538f7150a2b58b80c352d76c0a0f61701bd1f02
Dennis Given <dennis.given55@gmail.com>
2018-06-05 01:05:31 -0700

    Consolidate senate data service to be more config-driven

--- src/services/senate.ts
+++ src/services/senate.ts
@@ -1,7 +1,30 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 import { Element, ElementCompact, Options, xml2js, xml2json } from "xml-js";
 import ResponseFactory from "../factory/ResponseFactory";
+import { RollCallVote } from "../types/votes/RollCallVote";
 import { VoteSummary } from "../types/votes/VoteSummary";
+
+interface SenateServiceType<T> {
+  id: string;
+  modelType: T;
+  name: string;
+  url: string;
+}
+
+// TODO: Move into own config
+// TODO: modelType doesn't do much good here
+const serviceTypes: Array<SenateServiceType<any>> = [{
+  id: "roll-call-lists",
+  modelType: {} as VoteSummary,
+  name: "Roll Call Lists",
+  url: "roll_call_lists"
+} as SenateServiceType<VoteSummary>,
+{
+  id: "roll-call-lists",
+  modelType: {} as RollCallVote,
+  name: "Roll Call Votes",
+  url: "roll_call_votes"
+} as SenateServiceType<RollCallVote>];
 
 export class SenateDataService {
   // All inclusive values
@@ -10,7 +33,15 @@ export class SenateDataService {
   private static readonly CONGRESS_MIN: number = 101;
   private static readonly SESSION_MAX: number = 2;
   private static readonly SESSION_MIN: number = 1;
-
+  private static readonly VALUE_KEY: string = "value";
+  private static readonly DEFAULT_XMLJSON_CONFIG: Options.XML2JSON = {
+    cdataKey: SenateDataService.VALUE_KEY,
+    commentKey: SenateDataService.VALUE_KEY,
+    // TODO: flip this to false and transform properly to VoteSummary
+    compact: true,
+    nativeType: true,
+    textKey: SenateDataService.VALUE_KEY
+  } as Options.XML2JSON;
   private requestEngine: AxiosInstance = axios.create({
     baseURL: SenateDataService.BASE_SERVICE_URL,
     method: "post",
@@ -18,38 +49,54 @@ export class SenateDataService {
   });
 
   constructor() {
-    console.log(`SenateDataService initialized with config = [${this.requestEngine}]`);
+    console.log(
+      `SenateDataService initialized with config = [${this.requestEngine.toString()}]`
+    );
   }
   public getRollCallVotes = (
     congress: number = SenateDataService.CONGRESS_MAX,
     session: number = SenateDataService.SESSION_MAX
   ): Promise<VoteSummary> => {
-    const rollCallVoteXmlUrl: string = `roll_call_lists/${SenateDataService.constructXmlPath(congress, session)}`;
-    return this.requestEngine.request({
-      url: rollCallVoteXmlUrl
-    }).then((response: AxiosResponse) => {
-      let voteSummary: VoteSummary;
-      try {
+    const rollCallVotes: SenateServiceType<RollCallVote> = serviceTypes.filter((service) => service.id === "roll-call-votes")[0];
+    return this.getServiceResponse(rollCallVotes, congress, session);
+  };
+
+  public getRollCallLists = (
+    congress: number = SenateDataService.CONGRESS_MAX,
+    session: number = SenateDataService.SESSION_MAX
+  ): Promise<VoteSummary> => {
+    const rollCallLists: SenateServiceType<VoteSummary> = serviceTypes.filter((service) => service.id === "roll-call-lists")[0];
+    return this.getServiceResponse(rollCallLists, congress, session);
+  }
+
+  public getServiceResponse = (serviceType: SenateServiceType<any>, congress: number , session: number): Promise<any> => {
+    const relativeUrlWithXmlPath: string = `${serviceType.url}/${SenateDataService.constructXmlPath(congress, session)}`;
+    return this.requestEngine
+      .request({
+        url: relativeUrlWithXmlPath
+      })
+      .then((response: AxiosResponse) => {
+        let responseObj: any;
+        try {
           if (response.status === 200) {
-            const responseAsJson: string = xml2json(response.data, {
-              cdataKey: "value",
-              commentKey: "value",
-              // TODO: flip this to false and transform properly to VoteSummary
-              compact: true,
-              nativeType: true,
-              textKey: "value"
-            } as Options.XML2JSON);
+            const responseAsJson: string = xml2json(
+              response.data,
+              SenateDataService.DEFAULT_XMLJSON_CONFIG
+            );
             const processedResponse = JSON.parse(responseAsJson);
-            voteSummary = ResponseFactory.transformtXml2JsonResponse(processedResponse, "value") as VoteSummary;
+            responseObj = ResponseFactory.transformtXml2JsonResponse(
+              processedResponse,
+              SenateDataService.VALUE_KEY
+            ) as any;
           } else {
             console.error(response.data);
           }
         } catch (error) {
           console.error(error);
         }
-      return voteSummary;
+        return responseObj;
       });
-    }
+  };
 
   private static constructXmlPath = (
     congress: number = SenateDataService.CONGRESS_MAX,
