1d4c959a90783cbae08e2db4e22d78f240f5a9cb
Dennis Given <dennis.given55@gmail.com>
2019-01-27 14:22:07 -0800

    Add missing Member type

---
+++ src/types/members/Member.d.ts
@@ -0,0 +1,10 @@
+export interface Member {
+  firstName: string;
+  lastName: string;
+  listMemberId: string;
+  memberFull: string;
+  // TODO: Make enums
+  party: string;
+  state: string;
+  voteCast: string;
+}
